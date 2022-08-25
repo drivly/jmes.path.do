3f6f2a30a2c85b3f02811b4c189005ed1ac15999
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 09:46:21 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,6 +5,8 @@ export default {
     const { pathname, search } = new URL(req.url)
     const [_, encoded, ...target] = pathname.split('/')
     const path = decodeURI(encoded)
+    console.log('https://' + target.join('/') + search)
+    console.log({path})
     const json = await fetch('https://' + target.join('/') + search, req).then(res => res.json())
     return new Response(JSON.stringify(jmespath.search(json, path), null, 2), { headers: { 'content-type': 'application/json' }})
   }
