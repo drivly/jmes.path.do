0d609294091db71759b0faf10baa9bd67dcfc577
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 09:38:29 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,7 +3,8 @@ import jmespath from 'jmespath'
 export default {
   fetch: async req => {
     const { pathname, search } = new URL(req.url)
-    const [_, path, ...target] = pathname.split('/')
+    const [_, encoded, ...target] = pathname.split('/')
+    const path = decodeURI(encoded)
     const json = await fetch('https://' + target.join('/') + search, req).then(res => res.json())
     return new Response(JSON.stringify(jmespath.search({json, path}), null, 2), { headers: { 'content-type': 'application/json' }})
   }
