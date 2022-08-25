289cd131a264effcaf0dd24f0fbea8e5b3d981b8
Nathan Clevenger <nateclev@gmail.com>
2022-08-25 09:50:39 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,8 +2,8 @@ import jmespath from 'jmespath'
 
 export default {
   fetch: async req => {
-    const { pathname, search } = new URL(req.url)
-    const [_, encoded, ...target] = pathname.split('/')
+    const { origin, pathname, search } = new URL(req.url)
+    const [_, encoded, ...target] = req.url.replace(origin, '').split('/')
     const path = decodeURI(encoded)
     console.log('https://' + target.join('/') + search)
     console.log({path})
